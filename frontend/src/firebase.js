9a5df47ec872ad3f39225005d50c85e0b7505946
ChiragTanksali <chirag.tanksali@gmail.com>
2022-02-27 01:43:53 +0530

    Firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth , GoogleAuthProvider} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDArVa0UjpW7PqmrsMWcjsS82QvezLCmg4",
-  authDomain: "quora-clone-mern.firebaseapp.com",
-  projectId: "quora-clone-mern",
-  storageBucket: "quora-clone-mern.appspot.com",
-  messagingSenderId: "182383235813",
-  appId: "1:182383235813:web:fd9e0c34fcc0a4f07c7a0c",
-  measurementId: "G-62KKTN74PZ",
+  apiKey: "AIzaSyCTDAKMD72KoXdW8671_6ECrsCC2a2YvGU",
+  authDomain: "quora-clone-mern-1ca80.firebaseapp.com",
+  projectId: "quora-clone-mern-1ca80",
+  storageBucket: "quora-clone-mern-1ca80.appspot.com",
+  messagingSenderId: "745260272446",
+  appId: "1:745260272446:web:20a440f1dff366facbba6f",
+  measurementId: "G-BKHMTN95HH"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const analytics = getAnalytics(app)
 const provider = new GoogleAuthProvider();
 
 export { auth, provider };
